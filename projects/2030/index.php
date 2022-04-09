f53b2aba74cad40882ba1464d197c3b94bd92554
Flame36 <59798962+Flame36@users.noreply.github.com>
2022-04-09 07:13:29 +0200

    Added 2030 project

---
+++ projects/2030/index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <?php require $_SERVER["DOCUMENT_ROOT"] . "/templates/head.php" ?>
+
+    <title>Agenda 2030</title>
+
+    <link rel="stylesheet" href="/css/2030.css" />
+
+  </head>
+  <body>
+    <nav class="topbar">
+      <div class="topbar-container">
+          <a href="/">
+          <img src="/imgs/2030-logo.png" id="topbar-logo" />
+          </a>
+          <div class="topbar-toggle" id="mobile-menu">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+          </div>
+          <ul class="topbar-menu">
+          <li class="topbar-item">
+              <a href="/" class="topbar-link">Home</a>
+          </li>
+          <li class="topbar-button-container">
+              <a href="/login" class="topbar-button">Login</a>
+          </li>
+          </ul>
+      </div>
+      </nav>
+      <script src="/js/topbar.js"></script>
+    </nav>
+
+    <div class="main">
+      <div class="main-container">
+        <div class="article">
+          <h1>2030 agenda, goal 4: Better Instruction</h1>
+          
+          <h2>Credits</h2>
+          <p>
+            
+          </p>
+
+          <!-- AUTHOR: CIRO/FRANCESCO    WHAT (2030) -->
+          <h2>Wait a second, what's the 2030 agenda?</h2>
+          <p>
+          The 2030 agenda is a collection of goals, each focusing on a specific problem that afflicts the world.
+          That’s not all though! Every goal has specific targets, which are smaller subgoals that would be good to have, even if the main goal isn’t achieved.
+          There are a total of 17 goals and 169 targets (nice).
+          </p>
+        
+          <!-- AUTHOR: GIOSUE/FRANCESCO  WHO WHY WHEN WHERE (2030)-->
+          <h2 class="author-giosue">Who, why, when, and where</h2>
+          <p class="author-giosue">
+          The agenda was written on September 2015 by the United Nations, to guarantee a better future for our planet and it’s inhabitants.
+          Even though it was written in 2015 it only became official in 2016.
+          </p>
+
+          <!-- CONGIUNGIONE -->
+          <p></p>
+
+          <!-- AUTHOR: DONATO            WHAT (4) -->
+          <h2>Sounds good, but what's this goal 4 about?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+
+          <!-- AUTHOR: CIRO              WHEN WHERE (4)-->
+          <h2>Where are the problems?</h2>
+          <p>
+          Instruction and poverty are closely tied together: if you haven’t received the correct instruction you are not going to be able find a good job or to invoke your fundamental rights.
+          Along with poverty comes analphabetism, which affects hundreds of millions of people.
+          This is lots of times caused from dropping out of school at a very young age.
+          All of this problems are most pronounced in South/West Asia, Africa and South America.
+          </p>
+
+          <!-- AUTHOR: GIOVANNI          HOW (4)-->
+          <h2>I see, but how do we go about solving this?</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+
+          <!-- AUTHOR: GIOVANNI          IDEAS (4)-->
+          <h2>Couldn't we just...</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+
+          <!-- AUTHOR: GIOVANNI          CONCLUSION (4)-->
+          <h2>In conclusion</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+        </div>
+      </div>
+    </div>
+
+    <?php require $_SERVER["DOCUMENT_ROOT"] . "/templates/footer.php" ?>
+  </body>
+</html>
