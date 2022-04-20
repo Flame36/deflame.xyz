a28f51da86cb1b25c4b5ff684d18d7cb502132e1
Flame36 <59798962+Flame36@users.noreply.github.com>
2022-04-20 07:10:14 +0200

    Finalized 2030

--- projects/2030/index.php
+++ projects/2030/index.php
@@ -44,6 +44,7 @@
 
           <!-- AUTHOR: CIRO/FRANCESCO    WHAT (2030) -->
           <h2>Wait a second, what's the 2030 agenda?</h2>
+          <img src="/imgs/sdg.gif" id="img-1">
           <p>
           The 2030 agenda is a collection of goals, each focusing on a specific problem that afflicts the world.
           That’s not all though! Every goal has specific targets, which are smaller subgoals that would be good to have, even if the main goal isn’t achieved.
@@ -52,17 +53,18 @@
         
           <!-- AUTHOR: GIOSUE/FRANCESCO  WHO WHY WHEN WHERE (2030)-->
           <h2 class="author-giosue">Who, why, when, and where</h2>
+          <img src="/imgs/un-meeting.webp" id="img-1">
           <p class="author-giosue">
           The agenda was written on September 2015 by the United Nations, to guarantee a better future for our planet and it’s inhabitants.
           Even though it was written in 2015 it only became official in 2016.
           </p>
 
-          <!-- CONGIUNGIONE -->
-          <p></p>
-
           <!-- AUTHOR: DONATO            WHAT (4) -->
           <h2>Sounds good, but what's this goal 4 about?</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+          <p>
+          SDG 4 is meant to “ensure  inclusive  and  equitable  quality  education  and  promote  lifelong  learning  opportunities for all”.
+          What this means is that it’s about bringing everyone on the same plane when it comes to education, no matter economic or social status, along with bettering existing infrastructure.
+          </p>
 
           <!-- AUTHOR: CIRO              WHEN WHERE (4)-->
           <h2>Where are the problems?</h2>
@@ -75,15 +77,47 @@
 
           <!-- AUTHOR: GIOVANNI          HOW (4)-->
           <h2>I see, but how do we go about solving this?</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+          <img src="/imgs/4-5.gif" id="img-2">
+          <p>
+          Providing equal access to education goes hand in hand with addressing gender equality and poverty, as can be seen in goal 4’s targets.
+          Goal 4’s aim is to eliminate the distinction between genders and social classes when it comes to learning and having the ability to learn.
+          So more schools that are more affordable, more specialized, less biased and less unreachable (figuratively and literally).
+          </p>
 
           <!-- AUTHOR: GIOVANNI          IDEAS (4)-->
           <h2>Couldn't we just...</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+          <p>
+            <ul>
+              <li>create and upgrade structures, and make them a safe space for all people?</li>
+              <li>hire more teachers and school personnel?</li>
+              <li>incentivize rich countries into contributing funds?</li>
+            </ul>
+          <p>
+          <br>
+          or maybe:
+          </p>
+            <ul>
+            <br>
+              <li>teach sustainable living and promote eco-friendly lifestyles?</li>
+              <li>optimize structures for people with disabilities or special needs?</li>
+            </ul>
+          
+          <p>
+          <br>
+          The short answer is, of course, it depends.
+          But it mostly depends from you!
+          </p>
+          </p>
 
           <!-- AUTHOR: GIOVANNI          CONCLUSION (4)-->
           <h2>In conclusion</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, delectus incidunt! Vero ullam odio est ducimus iusto asperiores blanditiis eveniet! Dolores, atque officiis rerum consectetur obcaecati doloribus inventore tenetur dolor! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi optio consequatur magni tenetur cumque quasi maxime, quod animi, eius dolores magnam distinctio illum molestiae ullam necessitatibus culpa voluptas doloribus perferendis!</p>
+          <p>
+          Different people have wildly different ideas on how effective, if at all, all this effort will be.
+          Some say it'll just be all for nothing, while other choose to believe it'll guide us towards a better future, but in the end it's all up to you, your family, and your community to try and actually make the world a better place:
+          try to educate yourself on what work is being done, why, where, by who, how and, even if indirectly, try and change things up for the better.
+          That could be deciding to spread awareness, volunteer, or even simply vote for someone with more future concerned political views.
+          Do what you can and, even if just by a little, the world will be better.
+          </p>
         </div>
       </div>
     </div>
